3aae90f709489462495e0fbedc831a962d6e1fd5
Anuj guha <anuj.gh11@gmail.com>
2022-10-13 20:56:55 +0530

    Updates

---
+++ precache-manifest.b4c5f631f45f926d1c41f700390c4476.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "abdf0edd6db219935f1f",
+    "url": "/Portfolio/static/css/main.302d9f70.chunk.css"
+  },
+  {
+    "revision": "abdf0edd6db219935f1f",
+    "url": "/Portfolio/static/js/main.2c860bd1.chunk.js"
+  },
+  {
+    "revision": "a08b64f8a82ee0785fb2",
+    "url": "/Portfolio/static/js/runtime~main.0ae2b2e5.js"
+  },
+  {
+    "revision": "664846d42a8a1333be4c",
+    "url": "/Portfolio/static/js/2.dad6ffa9.chunk.js"
+  },
+  {
+    "revision": "61c75ad1c6ad002fdd6f",
+    "url": "/Portfolio/static/js/3.a50d1155.chunk.js"
+  },
+  {
+    "revision": "f30af8fb277074ee045495c9df3ac446",
+    "url": "/Portfolio/static/media/google.f30af8fb.png"
+  },
+  {
+    "revision": "b03e71e94f212008fcd22850dabddc3d",
+    "url": "/Portfolio/static/media/Infosys.b03e71e9.png"
+  },
+  {
+    "revision": "1482c8d226de80271aafc91523c9c299",
+    "url": "/Portfolio/static/media/isro.1482c8d2.png"
+  },
+  {
+    "revision": "90988c915e2f0075901f01cc9ff9a52a",
+    "url": "/Portfolio/static/media/coursera.90988c91.png"
+  },
+  {
+    "revision": "4cdacb8f89d588d69e8570edcbe49507",
+    "url": "/Portfolio/static/media/Poppins-SemiBold.4cdacb8f.ttf"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/Portfolio/static/media/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "f61a4eb27371b7453bf5b12ab3648b9e",
+    "url": "/Portfolio/static/media/Poppins-Medium.f61a4eb2.ttf"
+  },
+  {
+    "revision": "f6ea751e936ade6edcd03a26b8153b4a",
+    "url": "/Portfolio/static/media/Poppins-Light.f6ea751e.ttf"
+  },
+  {
+    "revision": "25cd0f688f815bc4f6ac2b71eb6278ba",
+    "url": "/Portfolio/static/media/Poppins-Thin.25cd0f68.ttf"
+  },
+  {
+    "revision": "544fa4f2678a8285eb88b8dfe503c90c",
+    "url": "/Portfolio/static/media/Poppins-ExtraBold.544fa4f2.ttf"
+  },
+  {
+    "revision": "a3e0b5f427803a187c1b62c5919196aa",
+    "url": "/Portfolio/static/media/Poppins-Bold.a3e0b5f4.ttf"
+  },
+  {
+    "revision": "d2a9c253cd9993b6d5372fb5b6a99b86",
+    "url": "/Portfolio/static/media/profilephoto.d2a9c253.png"
+  },
+  {
+    "revision": "3e347aa98598c5d7566aa73fc0a9017d",
+    "url": "/Portfolio/index.html"
+  }
+];
